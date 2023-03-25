07a45f0b3fd33163271eb04aea0243c31ee34aaf
convexy <64821530+convexy@users.noreply.github.com>
2023-03-25 20:41:14 +0900

    Add files via upload

--- rpa.py
+++ rpa.py
@@ -1,16 +1,38 @@
-from http.client import SWITCHING_PROTOCOLS
-import win32gui
+from time import time
+import win32con, win32gui
 
-handles = []
+class RPA:
+  def __init__(self, title, timeout=10):
+    self._title = title
+    self._timeout = timeout
 
-def a(hwnd, wh):
-  print(hwnd, win32gui.GetDlgCtrlID(hwnd))
-  print(win32gui.GetClassName(hwnd))
-  if win32gui.GetClassName(hwnd)=="WindowsForms10.EDIT.app.0.141b42a_r6_ad1":
-    handles.append([hwnd, win32gui.GetDlgItemText(wh, hwnd)])
+    self._hwnd = 0
+    def get_hwnd(hwnd, title):
+      if win32gui.GetWindowText(hwnd) == title:
+        self._hwnd = hwnd
+    win32gui.EnumWindows(get_hwnd, title)
+    if self._hwnd == 0:
+      raise RPAException("Window with title '" + self._title + "' not found.")
 
-wh = win32gui.FindWindow(None, "test")
-print(wh)
-win32gui.EnumChildWindows(wh, a, wh)
+  def activate(self):
+      start = time()
+      while True:
+        if win32gui.IsIconic(self._hwnd):
+          win32gui.ShowWindow(self._hwnd, win32con.SW_SHOW)
+        win32gui.SetForegroundWindow(self._hwnd)
+        win32gui.ShowWindow(self._hwnd, win32con.SW_SHOW)
+        # win32gui.SetWindowPos(self._hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOMOVE)
+        # win32gui.SetWindowPos(self._hwnd, win32con.HWND_NOTOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOMOVE | win32con.SWP_SHOWWINDOW)
+        if win32gui.GetForegroundWindow() == self._hwnd:
+          break
+        if time() - start > self._timeout:
+          raise RPAException("Window with title '" + self._title + "' not activated.")
 
-print(handles)
+    
+class RPAException(Exception):
+  pass
+
+if __name__ == "__main__":
+  rpa = RPA("test")
+  rpa.activate()
+  rpa.a()
